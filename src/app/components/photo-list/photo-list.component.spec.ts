9ca776f9fc592810bea268c1beaeaa3c5d0c251a
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2021-08-10 17:56:29 +0100

    * setup spectator * test input change events

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,25 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PhotoListComponent } from './photo-list.component';
+import {createComponentFactory, Spectator} from '@ngneat/spectator';
+import {HttpClient} from '@angular/common/http';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 describe('PhotoListComponent', () => {
-  let component: PhotoListComponent;
-  let fixture: ComponentFixture<PhotoListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PhotoListComponent ]
-    })
-    .compileComponents();
+  let spectator: Spectator<PhotoListComponent>;
+  const createComponent = createComponentFactory({
+    imports: [InfiniteScrollModule, RouterModule, FormsModule, ReactiveFormsModule],
+    component: PhotoListComponent,
+    mocks: [HttpClient]
   });
+
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PhotoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent()
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', (done) => {
+    const input = spectator.query('input') as Element;
+    spectator.typeInElement('some tricky keywords', input);
+
+    spectator.fixture.detectChanges();
+    spectator.fixture.whenStable().then(_ => {
+      done();
+      expect(spectator.component.spinner.value).toBeTrue();
+    })
   });
 });
