45dda8b33629eff8ddc8b9628e1a8dee05c0579b
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2021-08-11 14:02:57 +0100

    * checking service methods are called * add DEBUG to karma

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,24 +1,28 @@
 import { PhotoListComponent } from './photo-list.component';
-import {createComponentFactory, Spectator} from '@ngneat/spectator';
+import {createComponentFactory, mockProvider, Spectator} from '@ngneat/spectator';
 import {HttpClient} from '@angular/common/http';
 import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FlickrApiService} from '../../services/flickr-api.service';
+import {of} from 'rxjs';
 
 describe('PhotoListComponent', () => {
   let spectator: Spectator<PhotoListComponent>;
+
   const createComponent = createComponentFactory({
     imports: [InfiniteScrollModule, RouterModule, FormsModule, ReactiveFormsModule],
     component: PhotoListComponent,
-    mocks: [HttpClient]
+    mocks: [HttpClient, FlickrApiService]
   });
 
 
   beforeEach(() => {
-    spectator = createComponent()
+    spectator = createComponent({detectChanges: false})
   });
 
-  it('should create', (done) => {
+  it('should create set spinner off when keywords input', (done) => {
+    spectator.fixture.detectChanges();
     const input = spectator.query('input') as Element;
     spectator.typeInElement('some tricky keywords', input);
 
@@ -28,4 +32,37 @@ describe('PhotoListComponent', () => {
       expect(spectator.component.spinner.value).toBeTrue();
     })
   });
+
+  it('should call doPhotosReq and mapPhotos in service when keywords are input', (done) => {
+    const mockApiService = spectator.inject(FlickrApiService);
+    mockApiService.doPhotosReq.and.returnValue(of({photos: {photo: 'mock src'}}));
+
+    spectator.fixture.detectChanges();
+
+    const input = spectator.query('input') as Element;
+    spectator.typeInElement('some tricky keywords', input);
+
+    spectator.fixture.detectChanges()
+    spectator.fixture.whenStable().then(_ => {
+      done();
+      expect(mockApiService.doPhotosReq).toHaveBeenCalledWith('some tricky keywords', 'flickr.photos.search', 1);
+      expect(mockApiService.mapPhotos).toHaveBeenCalledWith('mock src');
+    })
+  });
+
+  it('should NOT call doPhotosReq service when keywords input is whitespace', (done) => {
+    const mockApiService = spectator.inject(FlickrApiService);
+    spectator.fixture.detectChanges();
+
+    const input = spectator.query('input') as Element;
+    spectator.typeInElement(' ', input);
+
+    spectator.fixture.detectChanges();
+    spectator.fixture.whenStable().then(_ => {
+      done();
+      expect(mockApiService.doPhotosReq).not.toHaveBeenCalled();
+      expect(mockApiService.mapPhotos).not.toHaveBeenCalled();
+    })
+  });
+
 });
