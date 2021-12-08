715ea94ffeb243b9a9a0e56ff114b69eb7f18972
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2021-12-08 09:23:44 +0000

    * auth0 prod settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  auth0Domain: 'dev-e9idq3tb.eu.auth0.com',
+  auth0Client: 'EiRKEfyXcdCuAXR9cIDnBuEJTCfxQD2d',
+  auth0ErrorPath: 'http://localhost:4200/error',
+  redirectUri: 'http://localhost:4200/photo-search',
+  production: true,
+  apiKey: '78dd5f0647f431627550a6cde4922067',
+  apiSecret: '88f2ac668588f8ec',
+  flickrUrl: 'https://www.flickr.com/services/rest/?'
 };
