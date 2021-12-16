0dc663e7058ce903b3119419fb8d88775b62e7b3
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2021-12-16 15:18:28 +0000

    * remove prod settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
   auth0ErrorPath: 'http://localhost:4200/error',
   redirectUri: 'http://localhost:4200/photo-search',
   production: false,
-  apiKey: '78dd5f0647f431627550a6cde4922067',
-  apiSecret: '88f2ac668588f8ec',
+  apiKey: '',
+  apiSecret: '',
   flickrUrl: 'https://www.flickr.com/services/rest/?'
 };
 
